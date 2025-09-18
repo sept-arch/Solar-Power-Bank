210e58b299e83d021f27d8cba60b47070309061a
sept-arch <leonardodguzman11@gmail.com>
2025-09-17 23:50:13 -0500

    Initial commit

---
+++ Solar.ino
@@ -0,0 +1,17 @@
+int clk = 12;
+int dt = 13;
+int yellow = 8;
+int red = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(dt, OUTPUT);
+  pinMode(clk, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
