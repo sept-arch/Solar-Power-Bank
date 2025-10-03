b8122f20c02492fb4a2d5d9db1e366e6c953f93d
sept-arch <leonardodguzman11@gmail.com>
2025-10-03 14:29:38 -0500

    Started working on control of the device with a rotary encoder. Early debugging with LEDs and Serial Print.

--- Powerbank/Solar.ino
+++ Powerbank/Solar.ino
@@ -1,17 +1,28 @@
-int clk = 12;
-int dt = 13;
-int yellow = 8;
-int red = 9;
+int RED = 9;
+int YELLOW = 8;
+int DT = 12;
+int CLK = 13;
+int lastStateCLK;
+int currentStateCLK;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(dt, OUTPUT);
-  pinMode(clk, OUTPUT);
-  pinMode(red, OUTPUT);
-  pinMode(yellow, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(CLK, INPUT); //CLK
+  pinMode(DT, INPUT); // DT
+
+  //for debugging
+  Serial.begin(9600);
+  // for comparison
+  lastStateCLK = digitalRead(13);
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+  currentStateCLK = digitalRead(CLK);
+  Serial.print(currentStateCLK);
+  Serial.print(digitalRead(DT));
 
 }
